fa462edd75f88415e2957a61e36d35da0a135bce
catnose-onair <nodding.com@gmail.com>
2020-10-21 23:06:35 +0900

    feat: show favicon on card

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,7 +3,11 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { PostItem } from "@src/types";
-import { getMemberByName, getHostFromURL } from "@src/utils/helper";
+import {
+  getMemberByName,
+  getHostFromURL,
+  getFaviconSrcFromHostname,
+} from "@src/utils/helper";
 
 dayjs.extend(relativeTime);
 
@@ -11,6 +15,8 @@ const PostLink: React.FC<{ item: PostItem }> = (props) => {
   const { authorName, title, isoDate, link } = props.item;
   const member = getMemberByName(authorName);
   if (!member) return null;
+
+  const hostname = getHostFromURL(link);
 
   return (
     <article className="post-link">
@@ -30,9 +36,19 @@ const PostLink: React.FC<{ item: PostItem }> = (props) => {
           </div>
         </a>
       </Link>
-      <a href={link}>
+      <a href={link} className="post-link__main-link">
         <h2 className="post-link__title">{title}</h2>
-        <div className="post-link__host">{getHostFromURL(link)}</div>
+        {hostname && (
+          <div className="post-link__site">
+            <img
+              src={getFaviconSrcFromHostname(hostname)}
+              width={14}
+              height={14}
+              className="post-link__site-favicon"
+            />
+            {hostname}
+          </div>
+        )}
       </a>
     </article>
   );
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,3 +6,6 @@ export function getHostFromURL(str: string) {
   const url = new URL(str);
   return url?.hostname || "blog";
 }
+export function getFaviconSrcFromHostname(hostname: string) {
+  return `http://www.google.com/s2/favicons?domain=${hostname}`;
+}
