8a95a399a7b6bbcb29b5bd86594362582e26c62f
Nishi Yuki <nishiyk256@gmail.com>
2021-11-24 12:08:59 +0900

    Fix siteRoot

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export const config = {
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://blog.ctf4.me"
+      ? "https://minicamp.ctf4.me"
       : "http://localhost:3000",
   headerLinks: [
     {
