cc821330c5d560ca94bc66c68312c5c9fb671aa2
sintaro <sintaronettt@gmail.com>
2021-12-15 20:56:26 +0900

    add: note rss追加

--- members.ts
+++ members.ts
@@ -18,7 +18,10 @@ export const members: Member[] = [
     bio:
       "学生時代よりソフトウェア開発・マーケティング業務に携わる。工学部卒（卒論AR実装)、農林水産省認定の６次産業での立ち上げ、D2Cブランドの立ち上げ、EC系SaaSの自社開発、フリーランスを経て、 Lisa Technologies(株)にジョイン",
     avatarSrc: "/avatars/shintaro_nakagawa.jpg",
-    sources: ["https://zenn.dev/sintaro/feed"],
+    sources: [
+      "https://zenn.dev/sintaro/feed",
+      "https://note.com/_sintaro0221_/rss",
+    ],
     twitterUsername: "sintaro0221",
     githubUsername: "sintaro",
   },
