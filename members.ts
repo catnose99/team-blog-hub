b33d58248b9f2c57557297e198671c6a55fcc7c5
nwiizo <syu.m.5151@gmail.com>
2022-03-09 12:26:16 +0900

    [WIP] FIX package.json

--- members.ts
+++ members.ts
@@ -2,18 +2,6 @@ import { Member } from "@src/types";
 
 export const members: Member[] = [
   {
-    id: "nwiizo",
-    name: "nwiizo",
-    role: "Software Developer",
-    bio: "Software Developer",
-    avatarSrc: "/avatars/nwiizo.jpeg",
-    sources: ["https://syu-m-5151.hatenablog.com/feed"],
-    includeUrlRegex: "syu-m-5151.hatenablog.com",
-    twitterUsername: "nwiizo",
-    githubUsername: "nwiizo",
-    websiteUrl: "https://nwiizo.github.io/",
-  },
-  {
     id: "yteraoka",
     name: "yteraoka",
     role: "SRE",
