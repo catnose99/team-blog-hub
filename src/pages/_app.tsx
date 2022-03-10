4c0f34aefc9693f15c9f1aefcf7329e16bd0317d
O-natsu <yuumi.natsubori@3-shake.com>
2022-03-10 14:48:56 +0900

    update: font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,8 +16,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           href={`${config.siteRoot}/logo.png`}
         />
         <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
         />
       </Head>
       <SiteHeader />
