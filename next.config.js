8f2a62bb72ebe83146b0a370b0506dae66d9e9cd
tyapama <tyapama@gmail.com>
2022-06-02 14:13:16 +0900

    add prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-    basePath: isProd ? '/chompy-developers-blog' : '',
-    assetPrefix: isProd ? '/chompy-developers-blog' : '',
+    basePath: isProd ? "/chompy-developers-blog" : "",
+    assetPrefix: isProd ? "/chompy-developers-blog/" : "",
     trailingSlash: true
 };
