99bf773127306f82c72af685bb1bf98146a89e5e
tyapama <tyapama@gmail.com>
2022-06-02 20:27:41 +0900

    fix assetPrefix path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
     basePath: isProd ? "/chompy-developers-blog" : "",
-    assetPrefix: isProd ? "/chompy-developers-blog/" : "",
+    assetPrefix: isProd ? "/chompy-developers-blog" : "",
     trailingSlash: true
 };
