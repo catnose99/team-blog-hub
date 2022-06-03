9042a2b3813d9eac3b774ddfd3e342c7a10ed252
tyapama <tyapama@gmail.com>
2022-06-03 16:14:25 +0900

    fix avatars url path

--- members.ts
+++ members.ts
@@ -1,5 +1,7 @@
 import { Member } from "@src/types";
 
+const urlPrefix = process.env.NODE_ENV === "production" ? '/chompy-developers-blog' : ''
+
 export const members: Member[] = [
   {
     id: "yagitatsu",
@@ -7,7 +9,7 @@ export const members: Member[] = [
     role: "CTO",
     bio:
       "パパエンジニア👨‍💻DeNA→メルカリ(ソウゾウ/メルペイ)→Chompy, Inc. (イマココ)。2019年に創業したChompy, Inc. という会社の取締役CTOやってます。",
-    avatarSrc: "/chompy-developers-blog/avatars/yagitatsu.jpeg",
+    avatarSrc: `${urlPrefix}/avatars/yagitatsu.jpeg`,
     sources: [
       "https://note.com/tatsuyayagi/rss",
       "https://qiita.com/yagitatsu/feed",
@@ -20,7 +22,7 @@ export const members: Member[] = [
     name: "Chompy Developer",
     bio:
         "Chompyのエンジニアアカウントです。技術関連のことやブログやPodcastの更新情報をつぶやきます💡 アプリの利用に関するお問い合わせはアプリ内の「困ったときは」よりお送りください。",
-    avatarSrc: "/chompy-developers-blog/avatars/chompy_developer.jpeg",
+    avatarSrc: `${urlPrefix}/avatars/chompy_developer.jpeg`,
     sources: [
     ],
     twitterUsername: "chompy_dev",
