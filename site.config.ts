abf939ffbfc600c8a087be9c6a3c19a038aa05ac
Saqoosha <a@saqoo.sh>
2022-10-18 13:02:19 +0900

    https://whatever-dev-blog.vercel.app/

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export const config = {
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub-whatever-inc.vercel.app/"
+      ? "https://whatever-dev-blog.vercel.app/"
       : "http://localhost:3000",
   headerLinks: [
     {
