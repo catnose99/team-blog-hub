3c6e3a4c2f766d3f2d667184745ae7e07f787f5a
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-05-25 10:19:57 +0900

    add satoken's profile

--- members.ts
+++ members.ts
@@ -413,4 +413,16 @@ export const members: Member[] = [
     githubUsername: "",
     websiteUrl: "",
   },
+  {
+    id: "satoken",
+    name: "satoken",
+    role: "SRE",
+    bio: "How do you like Wednesday?",
+    avatarSrc: "/avatars/satoken.jpèg",
+    sources: ["https://zenn.dev/satoken"],
+    includeUrlRegex: "",
+    twitterUsername: "",
+    githubUsername: "",
+    websiteUrl: "",
+  },
 ].sort((a, b) => (a.id < b.id ? -1 : 1));
