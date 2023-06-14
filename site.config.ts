722607aea6ab843e7821db40ba167caa1b3c91e1
Takanobu Izukawa <takanobu@izukawa.org>
2023-06-14 19:24:22 +0900

    最低限ローカルでの動作確認まで。
     * なんか色々そのままだと動かなかったので yarn.lock 生成し直したりした。
     * @types/react が最新版だと動かなかったので 18.0.1 を指定した。
     * 動作確認のため盛岡さんとひげのアカウントだけ追加した。

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,8 @@
 export const config = {
   siteMeta: {
-    title: "Team Blog Hub",
-    teamName: "catnose Inc.",
-    description: "RSS based blog starter kit for teams.",
+    title: "BASSDRUM TECH TEAM BLOG",
+    teamName: "BASSDRUM inc.",
+    description: "BASSDRUM inc. Tech Team Blog",
   },
   siteRoot:
     process.env.NODE_ENV === "production"
@@ -15,11 +15,11 @@ export const config = {
     },
     {
       title: "Company",
-      href: "https://zenn.dev/catnose99/articles/cb72a73368a547756862",
+      href: "https://bassdrum.org",
     },
     {
       title: "GitHub",
-      href: "https://github.com/catnose99/team-blog-hub",
+      href: "https://github.com/bassdrum-org",
     },
   ],
 };
